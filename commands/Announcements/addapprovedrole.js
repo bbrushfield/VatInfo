11091241cef911534ef5868a50e71ee23061c362
bbrushfield <bertie@brushfield.com>
2021-10-21 23:15:11 +0100

    Approved Role Command FINALIZATION

--- commands/Announcements/addapprovedrole.js
+++ commands/Announcements/addapprovedrole.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
-const { MessageMentions: { ROLES_PATTERN } } = require('discord.js');
+const { MessageMentions: { ROLES_PATTERN }} = require('discord.js');
+const Discord = require('discord.js');
+const { Permissions } = require('discord.js');
+const botconfig = require("./../../management/botconfig.json");
 
 function amendJson(id){
     console.log(id)
@@ -9,6 +12,7 @@ function amendJson(id){
     console.log('roles: ', roles)
     listjson = JSON.stringify(roles);
     fs.writeFileSync('commands/Announcements/approvedRoles.json',listjson,'utf-8');
+    return 'success'
 }
 
 function checkIfPresent(id){
@@ -23,6 +27,29 @@ function checkIfPresent(id){
     return false;
 }
 
+function SuccessEmbed(role, message){
+    const SuccEmbed = new Discord.MessageEmbed()
+    .setColor('GREEN')
+    .setTitle(`Success!`)
+    .setThumbnail(botconfig.IconURL)
+    .setDescription('The role has been added to the approval list!')
+    .addField(`Role name:`, `${role.name}`)
+    .addField(`Role ID`,`${role.id}`, true)
+    .addField(`Requested by:`, `${message.author.username}`, true)
+    .setFooter(botconfig.Footer, botconfig.IconURL)
+    message.channel.send({ embeds: [SuccEmbed] })
+}
+
+function Error(message, errormsg){
+    const ErrEmbed = new Discord.MessageEmbed()
+    .setColor('RED')
+    .setTitle(`Sorry!`)
+    .setDescription("There has been an error completing this command! If you feel this is wrong, please contact a member of the development team!")
+    .addField(`Unique Error Message:`, `${errormsg}`)
+    .setFooter(botconfig.Footer, botconfig.IconURL)
+    message.channel.send({ embeds: [ErrEmbed] })
+}
+
 module.exports = {
     name: "approve",
     aliases: ["addapprovedrole"],
@@ -30,20 +57,31 @@ module.exports = {
     description: "Adds an approved role to the list!",
     usage: "ping",
     run: async (client, message, args) => {
+        if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)){
+            Error(message, `You do not have the required permissions to complete this command!`)
+            return 
+        }
+
         if ( !args[0] ) {
-            return message.channel.send('Sorry! Please mention a role to add, or get the id!')
+            Error(message, `You have not given any role to process!`)
+            return 
         }
         const matches = message.content.match(ROLES_PATTERN)
-
-        if (!matches) return message.channel.send('Please mention a role to add, or get the ID!')
-
+        if (!matches) {
+            Error(message, `You have not mentioned a valid role!`)
+            return;
+            
+        } 
         var str = matches[0].replaceAll('<@&','').replaceAll('>','')
-        str = parseInt(str)
         console.log(str)
+        const role = message.guild.roles.cache.get(str)
         if (checkIfPresent(str) == true) {
-            return message.channel.send('This is already an approved role!')
+            Error(message, `The role you have mentioned is already approved!`)
+            return
         } else {
-            amendJson(str)
+            if (amendJson(str) == 'success') {
+                SuccessEmbed(role, message)
+            }
         }
 
     }
